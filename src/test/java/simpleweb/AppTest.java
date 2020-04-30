84b1619a7788bdb4cebcd481faa2e8458da11d3b
sudhirnikhade <sudhirnikhade@gmail.com>
2020-04-30 14:46:57 +0530

    First Commit with simple Hello world Java project

---
+++ src/test/java/simpleweb/AppTest.java
@@ -0,0 +1,20 @@
+package simpleweb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
